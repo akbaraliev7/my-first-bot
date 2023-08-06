7b8b36e59536e25ffdf8a87dc7f224cb265109a9
akbaraliev7 <akbaraliev129@gmail.com>
2023-08-06 20:06:04 +0600

    Telegram Bot

---
+++ config.py
@@ -0,0 +1,10 @@
+#By @K1p1k#
+#Downloaded from TG @End_soft
+#Leave this inscription#
+
+token='6342666883:AAHO8EDl_yQN2Abi_ZebGhAkVA-3XpNtap0' #https://t.me/BotFather TG @End_soft
+admin_id=int(1184283989) #https://t.me/getidsbot TG @End_soft
+
+#Автор: @K1p1k#
+#Загружено с TG @KiTools#
+#Оставь эту надпись#
